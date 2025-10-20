3445b07d5c44dc07807a7b2810378b2c245d3832
Vinicius Batista <vini.A.P.batista@gmail.com>
2025-10-20 15:21:48 -0300

    variaveis de ambiente configuradas

--- src/main/java/com/devsDoAgi/almoxarifado/AlmoxarifadoApplication.java
+++ src/main/java/com/devsDoAgi/almoxarifado/AlmoxarifadoApplication.java
@@ -1,5 +1,6 @@
 package com.devsDoAgi.almoxarifado;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlmoxarifadoApplication {
 
 	public static void main(String[] args) {
+        // Carrega o arquivo .env automaticamente (deve estar na raiz do projeto)
+        Dotenv dotenv = Dotenv.load();
+
+        // Define as variáveis no sistema, para o Spring conseguir ler no application.properties
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(AlmoxarifadoApplication.class, args);
 	}
 
